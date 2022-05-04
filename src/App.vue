8f6d7f55c7a9745dbb85fbf9d99b2b8410e3eb72
Stevekama <stevekamahertz@gmail.com>
2022-05-04 10:24:26 +0300

    Refactoring to use the backend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <Header 
-    @toggle-add-task="toggleAddTask" 
-    title="Tasks Tracker" 
-    :showAddTask="showAddTask"
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Tasks Tracker"
+      :showAddTask="showAddTask"
     />
     <div v-show="showAddTask">
       <AddTask @add-task="addTask" />
@@ -38,41 +38,61 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task];
+    async addTask(task) {
+      const res = await fetch("api/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm("Are you sure?")) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert("Something went wrong");
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder};
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updTask),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
+
+    async fetchTasks() {
+      const res = await fetch("api/tasks");
+      const data = await res.json();
+      return data;
+    },
+
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`); //fetching the task with the id
+      const data = await res.json();
+      return data;
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "March 20, 2020 at 10:00 AM",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: "Meeting at School",
-        day: "March 20, 2020 at 11:00 AM",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: "Food Shopping",
-        day: "March 20, 2020 at 12:00 AM",
-        reminder: false,
-      },
-    ];
+  async created() {
+    this.tasks = await this.fetchTasks();
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      '^/api':{
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/' },
+      }
+    }
+  },
   transpileDependencies: true
 })
